1c6581abb343ed54530744fb0082519eda822ef8
dogukanmuslukcu <dogukanmuslukcu@gmail.com>
2023-09-04 17:35:05 +0300

    renamed

--- Core.Persistence/Repositories/IAsyncRepository.cs
+++ Core.Persistence/Repositories/IAsyncRepository.cs
@@ -49,11 +49,11 @@ public interface IAsyncRepository<TEntity, TEntityId> : IQuery<TEntity>
         CancellationToken cancellationToken = default
         );
     Task<TEntity> AddAsync(TEntity entity);
-    Task<ICollection<TEntity>> AddRangeAsync(ICollection<TEntity> entity);
+    Task<ICollection<TEntity>> AddRangeAsync(ICollection<TEntity> entities);
     Task<TEntity> UpdateAsync(TEntity entity);
-    Task<ICollection<TEntity>> UpdateAsync(ICollection<TEntity> entity);
+    Task<ICollection<TEntity>> UpdateRangeAsync(ICollection<TEntity> entities);
     Task<TEntity> DeleteAsync(TEntity entity, bool permanent = false);
-    Task<ICollection<TEntity>> DeleteRangeAsync(ICollection<TEntity> entity, bool permanent = false);
+    Task<ICollection<TEntity>> DeleteRangeAsync(ICollection<TEntity> entities, bool permanent = false);
 
 
 
