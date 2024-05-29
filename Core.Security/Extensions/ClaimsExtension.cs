91bbfbe567186c5528b05fbd41c058cc68a67637
dogukanmuslukcu <dogukanmuslukcu@gmail.com>
2024-05-29 11:55:47 +0300

    Jwt implementation(*)=>

--- Core.Security/Extensions/ClaimsExtension.cs
+++ Core.Security/Extensions/ClaimsExtension.cs
@@ -19,6 +19,6 @@ public static class ClaimsExtension
     public static void AddNameIdentifier(this ICollection<Claim> claims, string nameIdentifier)
         => claims.Add(new Claim(ClaimTypes.NameIdentifier, nameIdentifier));
 
-    public static void AddName(this ICollection<Claim> claims, string[] roles)
+    public static void AddRoles(this ICollection<Claim> claims, string[] roles)
         => roles.ToList().ForEach(role => claims.Add(new Claim(ClaimTypes.Role, role)));
 }
